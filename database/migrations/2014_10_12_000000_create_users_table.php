178969b23fb3b06d995017d6224460e1a47d6bd4
Georgina <georginabosque@gmail.com>
2021-01-08 18:12:26 -0300

    Entry model (Create, Edit,Show)and factory, user profile page,Initial data whit Seeders

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,9 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('username')->nullable();
+            $table->string('twitter_username')->nullable();
+            //$table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Juan',
+            'email'=> 'juan@gmail.com',
+            'password'=> bcrypt('juan1234')
+
+        ]);
+
+        factory(User::class, 10)-> create();
+    }
+}
