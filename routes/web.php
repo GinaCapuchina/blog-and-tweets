4f0cc4af0f21ca272b40fbe40c8414d19e6eabee
Georgina <georginabosque@gmail.com>
2021-01-09 23:17:41 -0300

    Register EntryPolicy and authorize update

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ Route::get('/entries/create', 'EntryController@create');
 Route::post('/entries', 'EntryController@store');
 
 Route::get('/entries/{entryBySlug}', 'GuestController@show');
-
+//->middleware('can:update,entry');
 Route::get('/entries/{entry}/edit', 'EntryController@edit');
 Route::put('/entries/{entry}', 'EntryController@update');
 
